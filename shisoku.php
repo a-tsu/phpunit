639e302eb92a810547ecc151ca5a6c12dcf59061
a-tsu <lab_is_truth@yahoo.co.jp>
2015-09-16 23:38:48 +0900

    first commit

---
+++ shisoku.php
@@ -0,0 +1,35 @@
+<?php
+    class shisoku{
+	/**
+	 * 
+	 * @set (new shisoku(6, 2))
+	 * @assert (new shisoku->add(), 8) == true
+	 * 
+	 */
+        var $a, $b;
+        public function __construct($a,$b){
+		$this->setNum($a,$b);
+	    }
+        function setNum($a,$b){
+		$this->a = $a;
+		$this->b = $b;
+	}
+        function add(){
+		return $this->a + $this->b;
+	}
+	function sub(){
+		return $this->a - $this->b;
+	}
+	function mul(){
+		return $this->a * $this->b;
+	}
+	function div(){
+		return $this->a / $this->b;
+	}
+    }
+    $ans = new shisoku(6,2);
+    echo $ans->add()."\n";
+    echo $ans->sub()."\n";
+    echo $ans->mul()."\n";
+    echo $ans->div()."\n";
+?>
